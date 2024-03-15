60bc951b1e96d3d432c4937dea2d7d3286ef7860
Francois Gervais <0377892@cegepmontpetit.ca>
2024-03-15 16:34:17 -0400

    init

---
+++ Site_Core/Models/Employer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Site_Core.Models
+{
+    public class Employer
+    {
+        [Key]
+        [DisplayName(displayName: "Employer ID")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(255, ErrorMessage = "Name is too long")]
+        [DisplayName(displayName: "Employer Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(255, ErrorMessage = "Location is too long")]
+        [DisplayName(displayName: "Logo URL")]
+        public string Logo { get; set; }
+
+        [ValidateNever]
+        [DisplayName(displayName: "Jobs")]
+        public List<Job> Jobs { get; set; }
+    }
+}
---
+++ Site_Core/Models/Job.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site_Core.Models
+{
+    public class Job
+    {
+        [Key]
+        [DisplayName(displayName: "Job ID")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(255, ErrorMessage = "Title is too long")]
+        [DisplayName(displayName: "Job Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(255, ErrorMessage = "Description is too long")]
+        [DisplayName(displayName: "Job Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(255, ErrorMessage = "Location is too long")]
+        [DisplayName(displayName: "Job Location")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [DisplayName(displayName: "Job Start Date")]
+        public DateTime JobStart { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [DisplayName(displayName: "Job End Date")]
+        public DateTime JobEnd { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [DisplayName(displayName: "Skills Used")]
+        public List<Site_Utilities.TagsSkillsEnum> Skills { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [DisplayName(displayName: "Tools Used")]
+        public List<Site_Utilities.TagsToolsEnum> Tools { get; set; }
+
+        [Required(ErrorMessage = "This field is mandatory")]
+        [DisplayName(displayName: "Employer ID")]
+        [ForeignKey("Employer")]
+        public int EmployerId { get; set; }
+    }
+}
